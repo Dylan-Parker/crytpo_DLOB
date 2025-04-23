104c466c6b6a10d9747013edcce475f4c2f9cdcb
lji39 <127803233+lji39@users.noreply.github.com>
2025-04-22 20:31:05 -0500

    Update cnn_model.py

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -1,3 +1,5 @@
+#Reference: /notebooks/CNN Deeplearning v2 (4).ipynb
+#data processing and training available there
 
 from models.base_model import BaseModel
 import os
@@ -22,12 +24,14 @@ class CNNModel(nn.Module):
         super(CNNModel, self).__init__(config)
         # Define CNN layers based on crypto_lob.pdf / basic_cnn_model.ipynb [cite: 1, 5, 39, 94]
         # e.g., Conv2D -> Reshape -> Conv1D -> Pooling -> Dense [cite: 1, 5]
-        
+        self.D = config("D")
+        self.batch_size = config("batch_size")
+        self.nchannel=config("nchannel")
         # First Conv2D layer, input (batch_size, 1, 100, 40) -> output (batch_size, 16, 97, 1)
         self.conv2d = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(4, D))
         self.leaky_relu = nn.LeakyReLU(negative_slope=0.01)
         # Reshape layer (to reshape for Conv1D)
-        self.T = T  # Store T for dynamic reshaping
+        self.T = config("T")  # Store T for dynamic reshaping
         # First Conv1D layer
         self.conv1d_1 = nn.Conv1d(in_channels=16, out_channels=16, kernel_size=4)
         # Batch Normalization
